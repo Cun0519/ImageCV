b8e0c32767862840648b3df2df72c9a5366f9f27
CunXie <544842679@qq.com>
2020-01-12 22:54:06 +0800

    Initial Commit

---
+++ ImageCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ImageCV
+//
+//  Created by 谢存 on 2020/1/12.
+//  Copyright © 2020 cunxie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
