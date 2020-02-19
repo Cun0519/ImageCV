2158fd504c199170cca711e36b497c60031605cd
CunXie <544842679@qq.com>
2020-02-19 22:56:58 +0800

    删除Delegate

--- ImageCV/IrisCenterLocalizationDelegate.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  IrisCenterLocalizationDelegate.cpp
-//  ImageCV
-//
-//  Created by 谢存 on 2020/2/18.
-//  Copyright © 2020 cunxie. All rights reserved.
-//
-
-#include "IrisCenterLocalizationDelegate.hpp"
