11d4bce5541ce1acedba4c891260e29c75064bfa
CunXie <544842679@qq.com>
2020-03-17 22:25:38 +0800

    添加多线程

--- ImageCV/main.cpp
+++ ImageCV/main.cpp
@@ -9,12 +9,24 @@
 #include "Debug.hpp"
 #include "IrisCenterLocalizationPreProcess.hpp"
 #include "IrisCenterLocator.hpp"
+#include <thread>
 
 using namespace std;
 using namespace cv;
 
+void process(int returnValue[]);
+
 int main() {
+    //多线程
+    int returnValue[2];
+    thread t(process, returnValue);
+    t.join();
     
+    cout << returnValue[0] << " " << returnValue[1] << endl;
+    return 0;
+}
+
+void process(int returnValue[]) {
     // 基于当前系统的当前日期/时间
     time_t now = time(0);
     // 把 now 转换为字符串形式
@@ -34,6 +46,8 @@ int main() {
     Debug::debugDrawCross(eyeImage, irisCenter);
     //Debug::debugShow(eyeImage);
     imwrite(name, eyeImage);
-    cout << "irisCenter.x: " << irisCenter.x << "\nirisCenter.y: " << irisCenter.y << endl;
-    return 0;
+    //cout << "irisCenter.x: " << irisCenter.x << "\nirisCenter.y: " << irisCenter.y << endl;
+    
+    returnValue[0] = irisCenter.x;
+    returnValue[1] = irisCenter.y;
 }
